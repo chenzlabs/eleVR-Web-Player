026746110efd7b3bfeb3540df8466dad3a5c223c
M C <machenmusik@gmail.com>
2015-11-20 16:47:56 -0500

    another hls.js change

--- js/controls.js
+++ js/controls.js
@@ -300,7 +300,7 @@ var ccws = null;
         var hls = new Hls();
         // bind them together
         hls.attachMedia(video);
-        hls.on(Hls.Events.MSE_ATTACHED,function() {
+        hls.on(Hls.Events.MEDIA_ATTACHED,function() {
             console.log("video and hls.js are now bound together !");
             hls.loadSource(videoFile);
             hls.on(Hls.Events.MANIFEST_PARSED, function(event,data) {
